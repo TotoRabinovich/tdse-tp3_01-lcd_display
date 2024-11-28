dcd7043cc8f51261fe689374dfdb61836d943afa
Tomas Rabinovich <trabinovich@fi.uba.ar>
2024-11-28 12:57:29 -0300

    Trabajo terminado cuenta responsable

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main()
+{
+    printf("%s\n", "Hello World");
+}
